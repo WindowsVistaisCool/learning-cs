a564119fa3bc5fdc7f6d4254f00f77035e20c775
WindowsVistaisCool <kyle@quackings.com>
2022-05-17 22:45:53 -0400

    things

--- Program.cs
+++ Program.cs
@@ -3,27 +3,10 @@
 var account = new BankAccount("Mr. Bryan Walton", 110000);
 Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
 
-Console.WriteLine("tets");
-string? test = Console.ReadLine();
-if (test == null)
-{
-    Console.WriteLine("test");
-}
-else
-{
-    Console.WriteLine("test2");
-}
-
-return;
-
 while (true)
 {
     Console.WriteLine("Enter an option: ");
     string? option = Console.ReadLine();
-    if (option == null){
-        Console.WriteLine("invlaid");
-        continue;
-    }
     bool recievedQuitSig = false;
     switch (option)
     {
@@ -37,12 +20,32 @@ while (true)
             account.MakeDeposit(amount, date, note);
             Console.WriteLine($"Account {account.Number} was successfully deposited {amount} on {date} with a new balance of {account.Balance}.");
             break;
+        case "withdraw":
+            Console.WriteLine("Enter withdrawal amount: ");
+            amount = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter withdrawal date (MM/DD/YYYY): ");
+            date = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("Enter withdrawal note: ");
+            note = Console.ReadLine();
+            try
+            {
+                account.MakeWithdrawal(amount, date, note);
+                Console.WriteLine($"Account {account.Number} was successfully withdrawn {amount} on {date} with a new balance of {account.Balance}.");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            break;
+        case "balance":
+            Console.WriteLine($"The balance for account {account.Number} is {account.Balance}.");
+            break;
         case "quit":
             Console.WriteLine("Thank you for using the bank app!");
             recievedQuitSig = true;
             break;
         default:
-            Console.WriteLine("Invalid amount!");
+            Console.WriteLine("Invalid option!");
             break;
     }
     if (recievedQuitSig) break;
